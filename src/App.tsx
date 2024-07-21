48fd810cbeac65318c3365df78df30053cce7676
tuanhungdev1 <tuanhungdev274@gmail.com>
2024-07-21 12:26:56 +0700

    Add fonts and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="font-semibold underline">Hello world!</h1>;
 };
 
 export default App;
